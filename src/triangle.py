4accdc1e3ab566ecb5eb0815684c88750b6751b6
vadimchukhlatov <vchuhlatov@yandex.ru>
2023-11-15 22:55:42 +0200

    Добавил проверку на правильный треугольник

--- src/triangle.py
+++ src/triangle.py
@@ -6,9 +6,12 @@ class Triangle(Figure):
         super().__init__()
         self.sides = (side_a, side_b, side_c)
         self.check_params(*self.sides)
+        for side in self.sides:
+            if not sum(self.sides) - side > side:
+                raise ValueError("Cумма длин каждых двух сторон должна быть больше длины третьей стороны.")
 
     def get_area(self):
-        return sum(self.sides)/2
+        return sum(self.sides) / 2
 
     def get_perimeter(self):
         return sum(self.sides)
